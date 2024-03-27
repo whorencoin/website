2e8d4442f0d2b932852da0cf49ef259cbb4c2ebf
whorencoin <163061841+whorencoin@users.noreply.github.com>
2024-03-27 12:13:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function resize() {
-	const width = window.innerWidth / 16 - 2;
-	const count = Math.floor(width / 18);
-	document.body.style.setProperty("--meme-w", width / count - 2 + "rem");
-}
-
-window.addEventListener("load", function(){
-	resize();
-	window.addEventListener("resize", resize);
-});
+function resize(){const e=window.innerWidth/16-2,n=Math.floor(e/18);document.body.style.setProperty("--meme-w",e/n-2+"rem")}window.addEventListener("load",(function(){resize(),window.addEventListener("resize",resize)}));
